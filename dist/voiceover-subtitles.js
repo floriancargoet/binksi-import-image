1bbbd7e89e3260525228b4df3dd39ef07e6d6396
Florian Cargoët <florian.cargoet@gmail.com>
2023-10-16 11:46:49 +0200

    multiple texts per audio

--- dist/voiceover-subtitles.js
+++ dist/voiceover-subtitles.js
@@ -1,3 +1,7 @@
+/*
+//!CONFIG container-style (json) { "bottom": "10%", "textAlign": "center", "fontSize": "14px", "color": "white", "backgroundColor": "rgba(0, 0, 0, 0.6)", "borderRadius": "4px" }
+//!CONFIG subtitle-style (json) { "padding": "5px" }
+*/
 //! CODE_EDITOR
 const PLUGIN_NAME = "voiceover-subtitles";
 function setupEditorPlugin() {
@@ -6,71 +10,70 @@ function setupEditorPlugin() {
     EDITOR.loadedEditorPlugins.add(PLUGIN_NAME);
     const styleEl = document.createElement("style");
     document.head.append(styleEl);
-    styleEl.innerHTML = `
-    #play-tab-view .viewport-body {
-      height: 800px;
-    }`;
+    styleEl.innerHTML = ``;
 }
 if (EDITOR && !EDITOR.loadedEditorPlugins?.has(PLUGIN_NAME)) {
     setupEditorPlugin();
 }
 //! CODE_PLAYBACK
-const isInsideEditor = window.self.location.protocol === "blob:";
-const styleEl = document.createElement("style");
-document.head.append(styleEl);
-styleEl.innerHTML = `
-  #player {
-    /* transform: translate(-50%) scale(1.5) !important; */
-    /* transform-origin: top center; */
-    /* top: 0; */
-    overflow: visible;
-  }
-  #subtitles {
-    color: white;
-    text-align: center;
-  }
-`;
-if (isInsideEditor) {
-    // Show subtitles over the canvas
-    styleEl.innerHTML += `
-    #subtitles {
-      background-color: rgba(0, 0, 0, 0.3);
-      position: absolute;
-      bottom: 0;
-      width: 100%;
-    }
-    #subtitles > * {
-      padding: 10px;
-    }
-  `;
-}
 let subtitlesContainer;
 wrap.after(window, "start", () => {
     subtitlesContainer = document.createElement("div");
     subtitlesContainer.id = "subtitles";
-    ONE("#player").append(subtitlesContainer);
+    document.body.append(subtitlesContainer);
+    Object.assign(subtitlesContainer.style, { position: "absolute" }, FIELD(CONFIG, "container-style", "json"));
 });
-SCRIPTING_FUNCTIONS.PLAY_VO_SUB = function (field, event = this.EVENT) {
-    const soundFile = FIELD(event, field, "file");
-    const subText = (FIELD(event, field, "text") ??
-        FIELD(event, field, "dialogue"));
-    const subEl = document.createElement("div");
-    subEl.className = "subtitle";
-    subEl.innerHTML = subText.replace(/\n/g, "<br/>");
-    // Show the sub
-    subtitlesContainer.append(subEl);
+SCRIPTING_FUNCTIONS.PLAY_VO_SUB = async function (field, event = this.EVENT) {
+    const soundId = FIELD(event, field, "file");
+    const duration = await getSoundDuration(soundId);
+    const subTexts = (FIELDS(event, field, "text") ??
+        FIELDS(event, field, "dialogue"));
+    const configStyle = FIELD(CONFIG, "subtitle-style", "json");
+    const subs = subTexts.map((t) => {
+        const subEl = document.createElement("div");
+        subEl.className = "subtitle";
+        subEl.innerHTML = t.replace(/\n/g, "<br/>");
+        Object.assign(subEl.style, configStyle);
+        return {
+            el: subEl,
+            length: t.replace(/[\W]/g, "").length,
+        };
+    });
+    const totalLength = subs.reduce((total, sub) => total + sub.length, 0);
+    // Assume the audio will start immediately so we can already program the texts
+    let delay = 0;
+    for (const sub of subs) {
+        setTimeout(() => {
+            // Show the sub
+            subtitlesContainer.append(sub.el);
+        }, delay * 1000);
+        const subDuration = duration * (sub.length / totalLength);
+        delay += subDuration;
+        setTimeout(() => {
+            // Hide the sub
+            sub.el.remove();
+        }, delay * 1000);
+    }
     // Return a promise that will be resolved when the sound is finished and the sub is removed.
     return new Promise((resolve) => {
         const toRemove = this.ADD_ON_SOUND_END(() => {
             // Immediately unregister, so we are only called once.
             this.REMOVE_ON_SOUND_END(toRemove);
-            subEl.remove();
             resolve();
         }, "voiceover");
         // Now that the event handler is setup, play the sound
-        this.PLAY_SOUND(soundFile, "voiceover");
+        this.PLAY_SOUND(soundId, "voiceover");
     });
 };
+function getSoundDuration(soundId) {
+    return new Promise((resolve) => {
+        const a = new Audio();
+        a.addEventListener("loadedmetadata", () => {
+            resolve(a.duration);
+        });
+        a.src = PLAYBACK.getFileObjectURL(soundId);
+    });
+}
 class Queue {
     constructor() {
         this.running = false;
--- src/voiceover-subtitles/voiceover-subtitles.ts
+++ src/voiceover-subtitles/voiceover-subtitles.ts
@@ -13,6 +13,11 @@ declare global {
   }
 }
 
+/*
+//!CONFIG container-style (json) { "bottom": "10%", "textAlign": "center", "fontSize": "14px", "color": "white", "backgroundColor": "rgba(0, 0, 0, 0.6)", "borderRadius": "4px" }
+//!CONFIG subtitle-style (json) { "padding": "5px" }
+*/
+
 //! CODE_EDITOR
 const PLUGIN_NAME = "voiceover-subtitles";
 
@@ -23,10 +28,7 @@ function setupEditorPlugin() {
 
   const styleEl = document.createElement("style");
   document.head.append(styleEl);
-  styleEl.innerHTML = `
-    #play-tab-view .viewport-body {
-      height: 800px;
-    }`;
+  styleEl.innerHTML = ``;
 }
 
 if (EDITOR && !EDITOR.loadedEditorPlugins?.has(PLUGIN_NAME)) {
@@ -34,69 +36,78 @@ if (EDITOR && !EDITOR.loadedEditorPlugins?.has(PLUGIN_NAME)) {
 }
 
 //! CODE_PLAYBACK
-const isInsideEditor = window.self.location.protocol === "blob:";
-const styleEl = document.createElement("style");
-document.head.append(styleEl);
-styleEl.innerHTML = `
-  #player {
-    /* transform: translate(-50%) scale(1.5) !important; */
-    /* transform-origin: top center; */
-    /* top: 0; */
-    overflow: visible;
-  }
-  #subtitles {
-    color: white;
-    text-align: center;
-  }
-`;
-
-if (isInsideEditor) {
-  // Show subtitles over the canvas
-  styleEl.innerHTML += `
-    #subtitles {
-      background-color: rgba(0, 0, 0, 0.3);
-      position: absolute;
-      bottom: 0;
-      width: 100%;
-    }
-    #subtitles > * {
-      padding: 10px;
-    }
-  `;
-}
 
 let subtitlesContainer: HTMLDivElement;
 wrap.after(window, "start", () => {
   subtitlesContainer = document.createElement("div");
   subtitlesContainer.id = "subtitles";
-  ONE("#player").append(subtitlesContainer);
+  document.body.append(subtitlesContainer);
+  Object.assign(
+    subtitlesContainer.style,
+    { position: "absolute" },
+    FIELD(CONFIG, "container-style", "json")
+  );
 });
 
-SCRIPTING_FUNCTIONS.PLAY_VO_SUB = function (
+SCRIPTING_FUNCTIONS.PLAY_VO_SUB = async function (
   this: ScriptingThis,
   field: string,
   event = this.EVENT
 ) {
-  const soundFile = FIELD(event, field, "file");
-  const subText = (FIELD(event, field, "text") ??
-    FIELD(event, field, "dialogue")) as string;
-  const subEl = document.createElement("div");
-  subEl.className = "subtitle";
-  subEl.innerHTML = subText.replace(/\n/g, "<br/>");
-  // Show the sub
-  subtitlesContainer.append(subEl);
+  const soundId = FIELD(event, field, "file");
+  const duration = await getSoundDuration(soundId);
+
+  const subTexts = (FIELDS(event, field, "text") ??
+    FIELDS(event, field, "dialogue")) as Array<string>;
+  const configStyle = FIELD(CONFIG, "subtitle-style", "json");
+  const subs = subTexts.map((t) => {
+    const subEl = document.createElement("div");
+    subEl.className = "subtitle";
+    subEl.innerHTML = t.replace(/\n/g, "<br/>");
+    Object.assign(subEl.style, configStyle);
+    return {
+      el: subEl,
+      length: t.replace(/[\W]/g, "").length,
+    };
+  });
+
+  const totalLength = subs.reduce((total, sub) => total + sub.length, 0);
+  // Assume the audio will start immediately so we can already program the texts
+  let delay = 0;
+  for (const sub of subs) {
+    setTimeout(() => {
+      // Show the sub
+      subtitlesContainer.append(sub.el);
+    }, delay * 1000);
+    const subDuration = duration * (sub.length / totalLength);
+    delay += subDuration;
+    setTimeout(() => {
+      // Hide the sub
+      sub.el.remove();
+    }, delay * 1000);
+  }
+
   // Return a promise that will be resolved when the sound is finished and the sub is removed.
   return new Promise((resolve) => {
     const toRemove = this.ADD_ON_SOUND_END(() => {
       // Immediately unregister, so we are only called once.
       this.REMOVE_ON_SOUND_END(toRemove);
-      subEl.remove();
       resolve();
     }, "voiceover");
     // Now that the event handler is setup, play the sound
-    this.PLAY_SOUND(soundFile, "voiceover");
+    this.PLAY_SOUND(soundId, "voiceover");
   });
 };
+
+function getSoundDuration(soundId: string) {
+  return new Promise<number>((resolve) => {
+    const a = new Audio();
+    a.addEventListener("loadedmetadata", () => {
+      resolve(a.duration);
+    });
+    a.src = PLAYBACK.getFileObjectURL(soundId);
+  });
+}
 
 class Queue {
   running = false;
